7af94bd4597a227a35c6b5c6a0719e2e40af0dbf
MaXC1B3RS3C <maximsanchezporta@gmail.com>
2022-03-09 19:34:37 +0100

    Segon commit

---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require("express")
+var app = express()
+var db = require("./database.js")
+// Create express app
+var express = require("express")
+var app = express()
+
+// Server port
+var HTTP_PORT = 9000 
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Servidor escoltant a l'adreça http://localhost:%PORT%".replace("%PORT%",HTTP_PORT))
+});
+// Root endpoint
+app.get("/", (req, res, next) => {
+    res.json({"message":"Ok"})
+});
+
+
+// Insert here other API endpoints
+app.get("/api/users", (req, res, next) => {
+    var sql = "select * from user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
+app.get("/api/user/:id", (req, res, next) => {
+    var sql = "select * from user where id = " + req.params.id
+    db.get(sql, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+        }else{
+            res.json({
+                "message":"success",
+                "data":row
+            })
+        }
+      });
+});
+// Default response for any other request
+// Default response for any other request
+app.use(function (req, res) {
+    res.status(404).json({ "error": "Invalid endpoint" });
+});
