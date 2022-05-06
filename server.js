9ab2ad44f5dba609f7dc01be58531ef352cb331f
MaXC1B3RS3C <maximsanchezporta@gmail.com>
2022-05-06 05:03:40 +0200

    update comments

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const express = require('express');
 const app = express();
 const https = require('https');
 const fs = require('fs');
+//Paquete npm jsonwebtoken.
 const jwt = require('jsonwebtoken');
+//El método crypto.randomBytes() se utiliza para generar datos aleatorios artificiales criptográficamente bien construidos 
+//y la cantidad de bytes que se generarán en el código escrito.
+
 const jwtsecret = require('crypto').randomBytes(64).toString('hex')
-require("dotenv").config();	
+require("dotenv").config();
 // '09f26e402586e2faa8da4c98a35f1b20d6b033c6097befa8be3486a829587fe2f90a832bd3ff9d42710a4da095a2ce285b009f0c3730cd9b8e1af3eb84df6611
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
@@ -18,27 +22,30 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 app.use(bodyParser.json());
-// Vax a afegir un path de un fitxer html per a afegir 
-//el formulari que es troba en la part del client.
+
+// Paquete npm path.
 const path = require('path')
+
 //const serverToken=jwt.sign(username, process.env.jwtsecret, { expiresIn: '1800s' });	
 //res.cookie(serverToken);
 //res.redirect('/content')
+
 //Función flecha para la autenticación del token
 const auth = (req, res, next) => {
 	const token = req.cookies.access_token;
 	if (!token) {
-	  return res.sendStatus(403);
+		return res.sendStatus(403);
 	}
 	try {
-	  const data = jwt.verify(token,jwtsecret);
-	  req.userId = data.id;
-	  req.userRole = data.role;
-	  return next();
+		const data = jwt.verify(token, jwtsecret);
+		req.userId = data.id;
+		req.userRole = data.role;
+		return next();
 	} catch {
-	  return res.sendStatus(403);
+		return res.sendStatus(403);
 	}
-  };
+};
+
 // Root endpoint
 app.get("/", (req, res) => {
 	res.redirect("/loginform")
@@ -49,10 +56,12 @@ app.get("/", (req, res) => {
 app.get('/userform', (req, res) => {
 	res.sendFile(path.join(__dirname, './client/postuser.html'))
 })
+
 // Aqui añado la parte del formulario para loggear a usuarios
 app.get('/loginform', (req, res) => {
 	res.sendFile(path.join(__dirname, './client/login.html'))
 })
+
 //Endpoint POST para el login, cookies y jsonwebtoken
 app.post("/login", (req, res) => {
 	var errors = []
@@ -79,21 +88,24 @@ app.post("/login", (req, res) => {
 	var params = [data.name, data.email, data.password]
 	db.get('select * from user where name=?', params[0], function(err, row) {
 		if (!row) return res.send("No se ha encontrado el usuario");
-		if(err) return res.send("Error al iniciar sesión");
-		db.get('select * from user where name=? AND email = ? AND password = ? LIMIT 1', params[0], params[1],params[2], function(err, row) {
-		  if (!row) return res.send("Las credenciales no coinciden.");
-		  if(err) return res.send("Error al iniciar sesión");
-		  const token = jwt.sign({ id: 1, role: "ADMIN" }, jwtsecret);
+		if (err) return res.send("Error al iniciar sesión");
+		db.get('select * from user where name=? AND email = ? AND password = ? LIMIT 1', params[0], params[1], params[2], function(err, row) {
+			if (!row) return res.send("Las credenciales no coinciden.");
+			if (err) return res.send("Error al iniciar sesión");
+			const token = jwt.sign({
+				id: 1,
+				role: "ADMIN"
+			}, jwtsecret);
 			return res
-			.cookie("access_token", token, {
-			  httpOnly: true,
-			  secure: process.env.NODE_ENV === "production",
-			})
-			.status(200)
-			.redirect('/content')
+				.cookie("access_token", token, {
+					httpOnly: true,
+					secure: process.env.NODE_ENV === "production",
+				})
+				.status(200)
+				.redirect('/content')
 		});
 
-		
+
 	});
 });
 
@@ -107,9 +119,12 @@ app.get("/logout", auth, (req, res) => {
 			message: "Se cerró la sesión con exito "
 		});
 });
+// Endpoint de contenido (test a cookie)
+
 app.get('/content', auth, (req, res) => {
 	res.sendFile(path.join(__dirname, './client/content.html'))
 })
+
 //Users endpoint - Obtindre llista d'usuaris
 //Le quitare el "auth" a los endpoints de usuario para poder seguir consultando usuarios sin la cookie
 app.get("/api/users", (req, res, next) => {
@@ -128,6 +143,7 @@ app.get("/api/users", (req, res, next) => {
 		})
 	});
 });
+
 // id - endpoint - Obtindre un usuari per id
 app.get("/api/user/:id", auth, (req, res, next) => {
 	var sql = "select * from user where id = " + req.params.id
@@ -144,6 +160,7 @@ app.get("/api/user/:id", auth, (req, res, next) => {
 		}
 	});
 });
+
 //MATEIX ENDPOINT PARAMETRIZAT Y NO VULNERABLE A SQL-INJECTION
 //app.get("/api/user/:id", (req, res, next) => {
 // METODE NO SEGUR var sql = "select * from user where id = " + req.params.id
@@ -162,6 +179,7 @@ app.get("/api/user/:id", auth, (req, res, next) => {
 //    }
 //  });
 //});
+
 // body-parser endpoint - endpoint de creació de usuari
 //Le quitare el "auth" a los endpoints de usuario para poder seguir consultando usuarios sin la cookie
 app.post("/api/user/", auth, (req, res, next) => {
@@ -277,7 +295,6 @@ var HTTP_PORT = 9000
 //Para generar las claves en powershell e iniciar el servidor
 //Con express y https.createServer
 //Realizar comando mkcert para crear certificados 
-//Puede ser diferente en producción
 const server = https.createServer({
 	key: fs.readFileSync('./certs/localhost+2-key.pem'), // path to localhost+2-key.pem
 	cert: fs.readFileSync('./certs/localhost+2.pem'), // path to localhost+2.pem
