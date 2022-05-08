e03e96030ae4c228f9de4a0b40d51fbbf26c0a69
MaXC1B3RS3C <maximsanchezporta@gmail.com>
2022-05-08 02:09:07 +0200

    update repo

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ require("dotenv").config();
 // '09f26e402586e2faa8da4c98a35f1b20d6b033c6097befa8be3486a829587fe2f90a832bd3ff9d42710a4da095a2ce285b009f0c3730cd9b8e1af3eb84df6611
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
-//importar md5
-var md5 = require("md5")
 const bcrypt = require('bcryptjs')
 
 function bcrypthashing(password) {
